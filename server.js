a996342fe7509361661bc79f8fbe4645c76ae5ea
unknown <renannascimentoilha@gmail.com>
2026-04-02 17:50:14 -0300

    fix: adicionando DB_PORT e SSL final

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 14505, // <--- GARANTA QUE ISSO ESTÁ AQUI
     ssl: {
         rejectUnauthorized: false // <--- OBRIGATÓRIO PARA O AIVEN
     }
