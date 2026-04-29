33108728fa70df44dc1c91a887fab6f11d4d6fc5
unknown <renannascimentoilha@gmail.com>
2026-04-28 22:24:34 -0300

    atualizado

--- projetos.js
+++ projetos.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', async () => {
     const container = document.getElementById('container-projetos');
 
     try {
-        const resposta = await fetch('http://localhost:3000/projetos');
+        // Substitua a linha 5 do seu projetos.js por esta:
+        const resposta = await fetch('https://projeto-back-end-n8lm.onrender.com/projetos');
         const projetos = await resposta.json();
 
         if (projetos.length === 0) {
